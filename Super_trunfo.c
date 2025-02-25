888ae8a87bea6475c07ec1c1b2bf9d61afd07c7d
programacaojhon <robertalelis123@gmail.com>
2025-02-25 21:56:22 +0000

    em aberto

---
+++ Super_trunfo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+char estado_1, estado_2 [50];
+char cidade_1, cidade_2 [50];
+float populacao_1, populacao_2;
+float area_1, area_2;
+float pib_1, pib_2;
+float pontos_1, pontos_2;
+
+printf(" *** CARTA 1***\n");
+
+printf("Digite o nome do estado 1:\n");
+scanf("%s", &estado_1);
+printf("O estado 1 é: %s", estado_1);
+
+
+
+
+
+
+
+
+
+
+}
