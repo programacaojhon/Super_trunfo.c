2aa7b38caaa4cfdb81b83668474f0e5c135def9f
programacaojhon <robertalelis123@gmail.com>
2025-02-26 22:09:28 +0000

    nao imprime resultado

--- Super_trunfo.c
+++ Super_trunfo.c
@@ -2,21 +2,67 @@
 
 int main(){
 
-char estado_1, estado_2 [50];
-char cidade_1, cidade_2 [50];
-float populacao_1, populacao_2;
-float area_1, area_2;
-float pib_1, pib_2;
-float pontos_1, pontos_2;
+char estado,estd [100];
+char cidade,cdd [100];
+float populacao, pplc;
+float area,ar;
+float pib, pb;
+float pontos, pnts;
 
 printf(" *** CARTA 1***\n");
 
 printf("Digite o nome do estado 1:\n");
-scanf("%s", &estado_1);
-printf("O estado 1 é: %s", estado_1);
+scanf("%s", &estado);
 
+printf("Digite o nome da cidade 1: \n");
+scanf("%s", &cidade);
 
+printf("Digite a população da cidade 1: \n");
+scanf("%f", &populacao);
 
+printf("Digite a área da cidade 1: \n");
+scanf("%f", area);
+
+printf("Digite o PIB da cidade 1: \n");
+scanf("%f", &pib);
+
+printf("Digite o número de pontos turísticos da cidade 1: \n");
+scanf("%f", &pontos);
+
+printf(" *** CARTA 2***\n");
+
+printf("Digite o nome do estado 2:\n");
+scanf("%s", &estd);
+
+printf("Digite o nome da cidade 2: \n");
+scanf("%s", &cdd);
+
+printf("Digite a população da cidade 2: \n");
+scanf("%f", &pplc);
+
+printf("Digite a área da cidade 2: \n");
+scanf("%f", ar);
+
+printf("Digite o PIB da cidade 2: \n");
+scanf("%f", &pb);
+
+printf("Digite o número de pontos turísticos da cidade 2: \n");
+scanf("%f", &pnts);
+
+printf("O nome do estado da primeira carta é: %s \n", estado);
+printf("O nome da cidade da primeira carta é: %s \n", cidade);
+printf("A populaçao da cidade da primeira carta é: %f \n", populacao);
+printf("A área da cidade da primeira carta é: %f \n", area);
+printf("O PIB da cidade da primeira carta é: %f \n", pib),
+printf("O PIB da cidade da primeira carta é: %f \n", pib),
+printf("O número de pontos turísticos da cidade da primeira carta é: %f \n", pontos);
+
+printf("O nome do estado da segunda carta é: %s \n", estd);
+printf("O nome da cidade da segunda carta é: %s \n", cdd);
+printf("A populaçao da cidade da segunda carta é: %f \n", pplc);
+printf("A área da cidade da segunda carta é: %f \n", ar);
+printf("O PIB da cidade da segunda carta é: %f \n", pb),
+printf("O número de pontos turísticos da cidade da segunda carta é: %f \n", pnts);
 
 
 
