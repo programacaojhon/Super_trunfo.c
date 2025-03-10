1c1375525a35d477f5a012ca43e299cf3cdd44bd
programacaojhon <robertalelis123@gmail.com>
2025-03-10 22:27:26 +0000

    APENAS CARTA 1.C

--- Super_trunfo.c
+++ Super_trunfo.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-int main(){
 
-char estado,estd [100];
-char cidade,cdd [100];
-float populacao, pplc;
-float area,ar;
-float pib, pb;
-float pontos, pnts;
+    #include <stdio.h>
+
+    int main() {
+        char estado[100];
+        char cidade[100];
+        float populacao;
+        float area;
+        float pib;
+        float pontos;
+    
 
 printf(" *** CARTA 1***\n");
 
@@ -29,25 +32,7 @@ scanf("%f", &pib);
 printf("Digite o número de pontos turísticos da cidade 1: \n");
 scanf("%f", &pontos);
 
-printf(" *** CARTA 2***\n");
 
-printf("Digite o nome do estado 2:\n");
-scanf("%s", &estd);
-
-printf("Digite o nome da cidade 2: \n");
-scanf("%s", &cdd);
-
-printf("Digite a população da cidade 2: \n");
-scanf("%f", &pplc);
-
-printf("Digite a área da cidade 2: \n");
-scanf("%f", ar);
-
-printf("Digite o PIB da cidade 2: \n");
-scanf("%f", &pb);
-
-printf("Digite o número de pontos turísticos da cidade 2: \n");
-scanf("%f", &pnts);
 
 printf("O nome do estado da primeira carta é: %s \n", estado);
 printf("O nome da cidade da primeira carta é: %s \n", cidade);
@@ -57,17 +42,4 @@ printf("O PIB da cidade da primeira carta é: %f \n", pib),
 printf("O PIB da cidade da primeira carta é: %f \n", pib),
 printf("O número de pontos turísticos da cidade da primeira carta é: %f \n", pontos);
 
-printf("O nome do estado da segunda carta é: %s \n", estd);
-printf("O nome da cidade da segunda carta é: %s \n", cdd);
-printf("A populaçao da cidade da segunda carta é: %f \n", pplc);
-printf("A área da cidade da segunda carta é: %f \n", ar);
-printf("O PIB da cidade da segunda carta é: %f \n", pb),
-printf("O número de pontos turísticos da cidade da segunda carta é: %f \n", pnts);
-
-
-
-
-
-
-
-}
+return 0;
