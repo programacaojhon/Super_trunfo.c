cbb76264fe905125552155895fcb11f6bb58adb3
programacaojhon <robertalelis123@gmail.com>
2025-03-25 21:45:48 +0000

    teste1

---
+++ hora de codar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+    char produtoA [30] = "Produto A";
+    char produtoB [30] = "Produto B";
+    
+    unsigned int estoqueA = 1000;
+    unsigned int estoqueB = 2000;
+
+    float valorA = 10.50;
+    float valorB = 20.40;
+
+    unsigned int estoqueMinimoA = 500;
+    unsigned int estoqueMinimoB = 2500;
+
+    double valorTotalA;
+    double valorTotalB;
+
+
+
+
+
+return 0;
+
+}
